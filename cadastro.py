b739f3bcde5958468bff6807e190b9009bc4acfe
andradeluan <luanandrade2710@gmail.com>
2023-10-25 19:19:54 -0300

    menu atualizado

--- cadastro.py
+++ cadastro.py
@@ -2,6 +2,7 @@
 cliente_cadastrado = []
 nutri_cadastrado = []
 personal_cadastrado = []
+funcionario_cadastrado = []
 
 #função - cadastro de clientes
 def cad_cliente():
@@ -14,11 +15,11 @@ def cad_cliente():
     tel = input("Telefone: ")
     email = input("E-mail: ")
     matricula = tel[-4] + dt_nasc[-4]
+    cliente = {"nome": nome, "CPF": cpf, "Dt_Nasc": dt_nasc, "Endereço": end, "Telefone": tel, "E-mail": email, "Matrícula":matricula}
+    cliente_cadastrado.append(cliente)
     print('=-='*10)
     print("\nParabéns!\nCadastrado com sucesso.\n")
     print('=-='*10)
-    cliente = {"nome": nome, "CPF": cpf, "Dt_Nasc": dt_nasc, "Endereço": end, "Telefone": tel, "E-mail": email, "Matrícula":matricula}
-    cliente_cadastrado.append(cliente)
 
 #função - cadastro de nutricionista
 def cad_nutri():
@@ -32,11 +33,12 @@ def cad_nutri():
     email = input("E-mail: ")
     crn = input("CRN: ")
     matricula = tel[-4] + dt_nasc[-4]
+    nutri = {"nome": nome, "CPF": cpf, "Dt_Nasc": dt_nasc, "Endereço": end, "Telefone": tel, "E-mail": email, "CRN": crn, "Matrícula":matricula}
+    nutri_cadastrado.append(nutri)
     print('=-='*10)
     print("\nParabéns!\nCadastrado com sucesso.\n")
     print('=-='*10)
-    nutri = {"nome": nome, "CPF": cpf, "Dt_Nasc": dt_nasc, "Endereço": end, "Telefone": tel, "E-mail": email, "CRN": crn, "Matrícula":matricula}
-    nutri_cadastrado.append(nutri)
+    
 
 #função - cadastro de personal
 def cad_personal():
@@ -50,8 +52,23 @@ def cad_personal():
     email = input("E-mail: ")
     cref = input("CREF: ")
     matricula = tel[-4] + dt_nasc[-4]
+    personal = {"nome": nome, "CPF": cpf, "Dt_Nasc": dt_nasc, "Endereço": end, "Telefone": tel, "E-mail": email, "CREF": cref, "Matrícula":matricula}
+    personal_cadastrado.append(personal)
     print('=-='*10)
     print("\nParabéns!\nCadastrado com sucesso.\n")
     print('=-='*10)
-    personal = {"nome": nome, "CPF": cpf, "Dt_Nasc": dt_nasc, "Endereço": end, "Telefone": tel, "E-mail": email, "CREF": cref, "Matrícula":matricula}
-    personal_cadastrado.append(personal)
+
+#função - cadastro de funcionario
+def cad_funcionario():
+    print('=-='*10)
+    print("\nPREENCHA SEUS DADOS A SEGUIR\n")
+    nome = input("Nome: ")
+    cpf = input("CPF: ")
+    email = input("E-mail: ")
+    user = input('Crie um Login: ')
+    senha = input('Crie uma Senha: ')
+    funcionario = {"nome": nome, "CPF": cpf, "E-mail": email,"user": user, "senha":senha}
+    funcionario_cadastrado.append(funcionario)
+    print('=-='*10)
+    print("\nParabéns!\nCadastrado com sucesso.\n")
+    print('=-='*10)
--- menu.py
+++ menu.py
@@ -1,4 +1,7 @@
+import cadastro
 opcao = 0
+user = any
+senha = any
 while opcao != 3:
     #Menu Principal:
     print('=-='*10)
@@ -10,48 +13,70 @@ while opcao != 3:
 
     #Opção ENTRAR
     if opcao == 1:
-        print("login e senha")
+        while True:
+            verificador_user = input('Digite seu Login: ')
+            verificador_senha = input('Digite sua Senha: ')
+            for dicionario in cadastro.funcionario_cadastrado:
+                if verificador_user == dicionario["user"] and verificador_senha == dicionario["senha"]:
+                    print('=-='*10)
+                    print("\n[ 1 ] Cadastros\n[ 2 ] Nutricionista\n[ 3 ] Personal\n[ 4 ] Sair\n")
+                    print('=-='*10)
+                    opcao = int(input('\nEscolha uma opção: '))
+                    if opcao == 1:
+                        print("\nCADASTRAR COMO\n")
+                        print("\n [ 1 ] Cliente\n [ 2 ] Nutricionista\n [ 3 ] Personal\n [ 4 ] Menu Principal\n")
+                        cad = int(input("\nEscolha uma opção: "))
+                        print('=-='*10)
+                        while cad != 4:
+                            #chama a função de cadastro de clientes
+                            if cad == 1:
+                                #import cadastro
+                                cadastro.cad_cliente()
+                                cad = 4
+
+                            #chama função de cadastro de nutricionista
+                            elif cad == 2:
+                                #import cadastro
+                                cadastro.cad_nutri()
+                                cad = 4
+
+                            #chama função de cadastro de personal
+                            elif cad == 3:
+                                #import cadastro
+                                cadastro.cad_personal()
+                                cad = 4
+
+                            #Volta pro Menu Principal
+                            elif cad == 4:
+                                print('=-='*10)
+                                print("Voltando ao Menu Principal...")
+                                print('=-='*10)
+
+                            #nem um nem outro
+                            else:
+                                print('=-='*10)
+                                print("\nOpção inválida\n")
+                                print('=-='*10)
+                                cad = 4
+
+                    elif opcao == 2:
+                        pass
+                    #elif opcao == 3:
+                    elif opcao == 4:
+                        break
+                    else:
+                                print('=-='*10)
+                                print("\nOpção inválida\n")
+                                print('=-='*10)
+                else:
+                    print('Login ou Senha estão incorretos.\nTente novamente')
+
 
     #Opção CADASTRO
     elif opcao == 2:
-        print('=-='*10)
-        print("\nCADASTRAR COMO\n")
-        print("\n [ 1 ] Cliente\n [ 2 ] Nutricionista\n [ 3 ] Personal\n [ 4 ] Menu Principal\n")
-        cad = int(input("\nEscolha uma opção: "))
-        print('=-='*10)
+    
+        cadastro.cad_funcionario()
 
-        while cad != 4:
-            #chama a função de cadastro de clientes
-            if cad == 1:
-                import cadastro
-                cadastro.cad_cliente()
-                cad = 4
-
-            #chama função de cadastro de nutricionista
-            elif cad == 2:
-                import cadastro
-                cadastro.cad_nutri()
-                cad = 4
-            
-            #chama função de cadastro de personal
-            elif cad == 3:
-                import cadastro
-                cadastro.cad_personal()
-                cad = 4
-            
-            #Volta pro Menu Principal
-            elif cad == 4:
-                print('=-='*10)
-                print("Voltando ao Menu Principal...")
-                print('=-='*10)
-            
-            #nem um nem outro
-            else:
-                print('=-='*10)
-                print("\nOpção inválida\n")
-                print('=-='*10)
-                cad = 4
-        
     #Opção SAIR
     elif opcao == 3:
         print()
